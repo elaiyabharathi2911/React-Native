56f76a41b5f4954b70d57241f6be03d41fd924de
elaiyabharathi2911 <86315708+elaiyabharathi2911@users.noreply.github.com>
2021-11-24 16:35:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+// import DinamkImg from './Component/Nov02/DinamkImg';
+import Helloworld from './Component/Helloworld';
+// import Class from './Component/Class';
+
+
+export default function App() {
+  
+  return (
+    <View>
+       <Helloworld/> 
+      
+      
+    </View>
+  );
+}
+
